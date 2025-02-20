5fe48d6762f01c09a80c9615ee70f6bfb22b17ce
MikkelCornelius <mcb@cenimi.dk>
2025-02-20 12:49:49 +0100

    0.9.2.3
    Bug fix: Player IDs in manual input scores, was restricted to only 7 digits. Now with 8 digits any player ID can be written.
    
    Turns out Yokespai's formula is in fact not Zipf's law or z-sum. Seeding method has been renamed accordingly. I should support z-sum in the future, but too lazy rn.

--- lazer_bracket_generator.py
+++ lazer_bracket_generator.py
@@ -330,7 +330,7 @@ def add_player_manual_scores(m_root: tk.Toplevel, player_input_boxes: list, map_
     if team_vs:
         textbox = ctk.CTkEntry(m_root, width=200)
     else:
-        textbox = ctk.CTkEntry(m_root, width=100, validate='key', validatecommand=(outer_root.register(entry_chararcter_limit_creator(7)), '%P'))
+        textbox = ctk.CTkEntry(m_root, width=100, validate='key', validatecommand=(outer_root.register(entry_chararcter_limit_creator(8)), '%P'))
     textbox.grid(row=4+len(player_input_boxes), column=0, padx=(5, 40), pady=GUI_yspacing, sticky='w')
     player_input_boxes.append(textbox)
 
@@ -959,7 +959,7 @@ def get_seeding(scores: np.ndarray, seeding_method: str, mappool_size: int) -> n
     map_seeds_out = map_seeds
     if seeding_method=="Total Score":
         map_seeds = scores[:, 1:]
-    if seeding_method=="Zipf's Law":
+    if seeding_method=="Yokespai's Formula":
         row_sums = np.sum(100/(map_seeds+mappool_size*1.4), axis=1)
         sorted_indices = np.argsort(row_sums)[::-1]
     else:
@@ -970,7 +970,8 @@ def get_seeding(scores: np.ndarray, seeding_method: str, mappool_size: int) -> n
     else:
         final_seed[sorted_indices] = np.arange(len(sorted_indices), 0, -1)
     seeding = np.column_stack([scores[:,0], map_seeds, final_seed])
-    if seeding_method=="Zipf's Law":
+
+    if seeding_method=="Yokespai's Formula":
         seed_score = row_sums
     elif seeding_method=="Average Rank":
         seed_score = map_seeds.sum(1)/mappool_size
@@ -1977,7 +1978,7 @@ def create_bracket(api: Ossapi, mp: list, number_of_qualifying_players: int, red
         map_list = get_map_list(api, mp[0])
         included_mods = get_included_mods(api, mp[0])
         mod_count = get_mod_count(api, mp[0], included_mods)
-        
+
         #request scores
         print('2/4: requesting scores..')
         GUI_terminal_print('2/4: requesting scores..')
@@ -2081,7 +2082,7 @@ def create_bracket(api: Ossapi, mp: list, number_of_qualifying_players: int, red
                     if teamname[:3].upper() in dublicated_acronyms:
                         teams_with_dublicated_acronyms.append(teamnames.index(teamname))
                 acronym_solver(teams_with_dublicated_acronyms, True)
-        else: 
+        else:
             acronyms = []
             dublicated_acronyms = []
             for player in player_data: #find all dublicated acronyms
@@ -2302,7 +2303,7 @@ ToolTip(team_vs_input_help, 'You can just copy your screening file. The format s
 #create seeding method menu
 seeding_method_label = ctk.CTkLabel(settings_frame, text="Seeding method")
 seeding_method_label.grid(padx=GUI_xspacing, pady=(5,0), sticky='w')
-seeding_methods = ["Total Score", "Average Rank", "Zipf's Law"]
+seeding_methods = ["Total Score", "Average Rank", "Yokespai's Formula"]
 seeding_method_menu = ctk.CTkComboBox(settings_frame, values=seeding_methods)
 seeding_method_menu.grid(padx=GUI_xspacing, pady=(0,5), sticky='w')
 seeding_method_menu.set("Total Score")
